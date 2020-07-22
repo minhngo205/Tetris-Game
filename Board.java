0cc4fae1751740dbb553bd1698139582822f4522
= <=>
2020-07-22 16:51:29 +0700

    Edit Board file

--- Board.java
+++ Board.java
@@ -1,174 +1,242 @@
-import javax.imageio.ImageIO;
-import javax.swing.JPanel;
-import javax.swing.JLabel;
-import java.awt.Graphics;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.image.BufferedImage;
-import java.io.IOException;
-import javax.swing.Timer;
-//import java.awt.event.KeyListener;
-import java.awt.event.KeyAdapter;
-/**
- * Board
- */
-public class Board extends JPanel /* implements KeyListener  */{
+import java.awt.*;
+import java.awt.event.*;
+import static java.lang.Math.*;
+import static java.lang.String.format;
+import java.util.*;
+import javax.swing.*;
+
+public class Board extends JPanel implements Runnable {
 
     private static final long serialVersionUID = -8715353373678321308L;
-
-    private BufferedImage blocks;
-    private final int blockSize = 30;
-    private final int boardWidth = 10, boardHeight = 20;
-    private int[][] board = new int[boardHeight][boardWidth];
-
-    private Shape[] shapes = new Shape[7];
-    private Shape currentShape;
-    private Timer timer;
-    private final int FPS = 60;
-    private final int delay = 1000 / FPS;
-    private boolean gameOver = false;
-    //private int numLinesRemove = 0;
-    private JLabel statusbar;
-
-    public Board(Window parent) {
-        try {
-            blocks = ImageIO.read(Board.class.getResource("tiles.png"));
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+    
+    enum Dir {
+        right(1, 0), down(0, 1), left(-1, 0);
+ 
+        Dir(int x, int y) {
+            this.x = x;
+            this.y = y;
         }
+        final int x, y;
+    };
+ 
+    public static final int EMPTY = -1;
+    public static final int BORDER = -2;
+ 
+    Shape fallingShape;
+    Shape nextShape;
+ 
+    // position of falling shape
+    int fallingShapeRow;
+    int fallingShapeCol;
 
-        timer = new Timer(delay, new ActionListener() {
+    final static int nRows = 20;
+    final static int nCols = 12;
+    final int[][] grid = new int[nRows][nCols];
+ 
+    Thread fallingThread;
+    final Scoreboard scoreboard = new Scoreboard();
+    static final Random rand = new Random();
+ 
+    public Board() {
+ 
+        addKeyListener(new KeyAdapter() {
+            boolean fastDown;
+ 
             @Override
-            public void actionPerformed(java.awt.event.ActionEvent e) {
-                // TODO Auto-generated method stub
-                update();
+            public void keyPressed(KeyEvent e) {
+ 
+                if (scoreboard.isGameOver())
+                    return;
+ 
+                switch (e.getKeyCode()) {
+ 
+                    case KeyEvent.VK_UP:
+                        if (canRotate(fallingShape))
+                            rotate(fallingShape);
+                        break;
+ 
+                    case KeyEvent.VK_LEFT:
+                        if (canMove(fallingShape, Dir.left))
+                            move(Dir.left);
+                        break;
+ 
+                    case KeyEvent.VK_RIGHT:
+                        if (canMove(fallingShape, Dir.right))
+                            move(Dir.right);
+                        break;
+ 
+                    case KeyEvent.VK_DOWN:
+                        if (!fastDown) {
+                            fastDown = true;
+                            while (canMove(fallingShape, Dir.down)) {
+                                move(Dir.down);
+                                repaint();
+                            }
+                            shapeHasLanded();
+                        }
+                }
                 repaint();
             }
-        });
-
-
-        timer.start();
-        // shape
-        shapes[0] = new Shape(blocks.getSubimage(0, 0, blockSize, blockSize), new int[][] { { 1, 1, 1, 1 } // I shape
-        }, this, 1);
-        shapes[1] = new Shape(blocks.getSubimage(blockSize, 0, blockSize, blockSize),
-                new int[][] { { 1, 1, 0 }, { 0, 1, 1 } // Z shape
-                }, this, 2);
-        shapes[2] = new Shape(blocks.getSubimage(blockSize * 2, 0, blockSize, blockSize),
-                new int[][] { { 0, 1, 1 }, { 1, 1, 0 } // S shape
-                }, this, 3);
-        shapes[3] = new Shape(blocks.getSubimage(blockSize * 3, 0, blockSize, blockSize),
-                new int[][] { { 1, 1, 1 }, { 0, 0, 1 } // J shape
-                }, this, 4);
-        shapes[4] = new Shape(blocks.getSubimage(blockSize * 4, 0, blockSize, blockSize),
-                new int[][] { { 1, 1, 1 }, { 1, 0, 0 } // L shape
-                }, this, 5);
-        shapes[5] = new Shape(blocks.getSubimage(blockSize * 5, 0, blockSize, blockSize),
-                new int[][] { { 1, 1, 1 }, { 0, 1, 0 } // T shape
-                }, this, 6);
-        shapes[6] = new Shape(blocks.getSubimage(blockSize * 6, 0, blockSize, blockSize),
-                new int[][] { { 1, 1 }, { 1, 1 } // O shape
-                }, this, 7);
-
-        setNextShape();
-        initBoard(parent);
-    }
-
-    private void initBoard(Window parent) {
-
-        setFocusable(true);
-        statusbar = parent.getStatusBar();
-        addKeyListener(new TAdapter());
-    }
-
-    public void update(){
-        currentShape.update();
-        if(gameOver){
-            timer.stop();
-        }
-    }
-
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        currentShape.render(g);
-        
-        for (int row = 0; row < board.length; row++) {
-            for (int col = 0; col < board[row].length; col++) {
-                if(board[row][col] != 0)
-                    g.drawImage(blocks.getSubimage((board[row][col] - 1)*blockSize, 0, blockSize, blockSize), col*blockSize, row*blockSize, null);
+ 
+            @Override
+            public void keyReleased(KeyEvent e) {
+                fastDown = false;
             }
-            
-        }
-        g.drawRect(0,0,blockSize*boardWidth,blockSize*boardHeight);
-
-        for (int i = 0; i < boardHeight; i++) {
-            g.drawLine(0, i * blockSize, boardWidth * blockSize, i * blockSize);
-        }
-        for (int j = 0; j < boardWidth; j++) {
-            g.drawLine(j * blockSize, 0, j * blockSize, boardHeight * blockSize);
+        });
+    }
+ 
+    void selectShape() {
+        fallingShapeRow = 1;
+        fallingShapeCol = 5;
+        fallingShape = nextShape;
+        Shape[] shapes = Shape.values();
+        nextShape = shapes[rand.nextInt(shapes.length)];
+        if (fallingShape != null)
+            fallingShape.reset();
+    }
+ 
+    void startNewGame() {
+        stop();
+        initGrid();
+        selectShape();
+        scoreboard.reset();
+        (fallingThread = new Thread(this)).start();
+    }
+ 
+    void stop() {
+        if (fallingThread != null) {
+            Thread tmp = fallingThread;
+            fallingThread = null;
+            tmp.interrupt();
         }
     }
+ 
+    void initGrid() {
+        for (int r = 0; r < nRows; r++) {
+            Arrays.fill(grid[r], EMPTY);
+            for (int c = 0; c < nCols; c++) {
+                if (c == 0 || c == nCols - 1 || r == nRows - 1)
+                    grid[r][c] = BORDER;
+            }
+        }
+    }
+ 
+    @Override
+    public void run() {
+ 
+        while (Thread.currentThread() == fallingThread) {
+ 
+            try {
+                Thread.sleep(scoreboard.getSpeed());
+            } catch (InterruptedException e) {
+                return;
+            }
+ 
+            if (!scoreboard.isGameOver()) {
+                if (canMove(fallingShape, Dir.down)) {
+                    move(Dir.down);
+                } else {
+                    shapeHasLanded();
+                }
+                repaint();
+            }
+        }
+    }
+ 
+    
 
-    public void setNextShape(){
-        int index = (int)(Math.random()*shapes.length);
-
-        Shape newShape = new Shape(shapes[index].getBlock(), shapes[index].getCoords(), this, shapes[index].getColor());
-        currentShape = newShape;
-
-        for (int row = 0; row < currentShape.getCoords().length; row++) {
-            for (int col = 0; col < currentShape.getCoords()[row].length; col++) {
-                if(currentShape.getCoords()[row][col] != 0){
-                    if(board[row][col + 3] != 0)
-                        gameOver = true;
+ 
+    boolean canRotate(Shape s) {
+        if (s == Shape.Square)
+            return false;
+ 
+        int[][] pos = new int[4][2];
+        for (int i = 0; i < pos.length; i++) {
+            pos[i] = s.pos[i].clone();
+        }
+ 
+        for (int[] row : pos) {
+            int tmp = row[0];
+            row[0] = row[1];
+            row[1] = -tmp;
+        }
+ 
+        for (int[] p : pos) {
+            int newCol = fallingShapeCol + p[0];
+            int newRow = fallingShapeRow + p[1];
+            if (grid[newRow][newCol] != EMPTY) {
+                return false;
+            }
+        }
+        return true;
+    }
+ 
+    void rotate(Shape s) {
+        if (s == Shape.Square)
+            return;
+ 
+        for (int[] row : s.pos) {
+            int tmp = row[0];
+            row[0] = row[1];
+            row[1] = -tmp;
+        }
+    }
+ 
+    void move(Dir dir) {
+        fallingShapeRow += dir.y;
+        fallingShapeCol += dir.x;
+    }
+ 
+    boolean canMove(Shape s, Dir dir) {
+        for (int[] p : s.pos) {
+            int newCol = fallingShapeCol + dir.x + p[0];
+            int newRow = fallingShapeRow + dir.y + p[1];
+            if (grid[newRow][newCol] != EMPTY)
+                return false;
+        }
+        return true;
+    }
+ 
+    void shapeHasLanded() {
+        addShape(fallingShape);
+        if (fallingShapeRow < 2) {
+            scoreboard.setGameOver();
+            scoreboard.setTopscore();
+            stop();
+        } else {
+            scoreboard.addLines(removeLines());
+        }
+        selectShape();
+    }
+ 
+    int removeLines() {
+        int count = 0;
+        for (int r = 0; r < nRows - 1; r++) {
+            for (int c = 1; c < nCols - 1; c++) {
+                if (grid[r][c] == EMPTY)
+                    break;
+                if (c == nCols - 2) {
+                    count++;
+                    removeLine(r);
                 }
             }
         }
-
-
+        return count;
     }
-
-    public JLabel getboardSTT(){
-        return statusbar;
-    }
-
-    public int getBlockSize() {
-        return blockSize;
-    }
-
-    public int[][] getBoard(){
-        return board;
-    }
-    class TAdapter extends KeyAdapter {
-        @Override
-        public void keyTyped(KeyEvent e) {
-            // TODO Auto-generated method stub
-
-        }
-
-        @Override
-        public void keyPressed(KeyEvent e) {
-            // TODO Auto-generated method stub
-            if(e.getKeyCode() == KeyEvent.VK_LEFT){
-                currentShape.setDeltaX(-1);
-            }
-            if(e.getKeyCode() == KeyEvent.VK_RIGHT){
-                currentShape.setDeltaX(1);
-            }
-            if(e.getKeyCode() == KeyEvent.VK_DOWN){
-                currentShape.speedDown();
-            }
-            if(e.getKeyCode() == KeyEvent.VK_UP){
-                currentShape.rotate();
-            }
-        }
-
-        @Override
-        public void keyReleased(KeyEvent e) {
-            // TODO Auto-generated method stub
-            if(e.getKeyCode() == KeyEvent.VK_DOWN){
-                currentShape.normalSpeed();
-            }
+ 
+    void removeLine(int line) {
+        for (int c = 0; c < nCols; c++)
+            grid[line][c] = EMPTY;
+ 
+        for (int c = 0; c < nCols; c++) {
+            for (int r = line; r > 0; r--)
+                grid[r][c] = grid[r - 1][c];
         }
     }
+    
+    void addShape(Shape s) {
+        for (int[] p : s.pos)
+            grid[fallingShapeRow + p[1]][fallingShapeCol + p[0]] = s.ordinal();
+    }
+ 
 }
